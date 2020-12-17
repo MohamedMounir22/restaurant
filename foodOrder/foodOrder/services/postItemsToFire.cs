adcbfdd6a39a1edb7fcc876126df385c46749f75
mohamedMounir <mohammedmonir2254@gmail.com>
2020-12-17 17:07:02 +0200

    new

--- foodOrder/foodOrder/services/postItemsToFire.cs
+++ foodOrder/foodOrder/services/postItemsToFire.cs
@@ -1,10 +1,57 @@
-﻿using System;
+﻿using Firebase.Database;
+using foodOrder.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace foodOrder.services
 {
    public class postItemsToFire
     {
-    }
+        FirebaseClient firbaseClient;
+        ObservableCollection<items> allitmes;
+
+        public postItemsToFire ()
+        {
+            firbaseClient = new FirebaseClient("https://tryingsecondtime-d3862-default-rtdb.firebaseio.com/");
+            allitmes = new ObservableCollection<items>() {
+                new items{}
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                }
+        }
+
+
+
+
+
+        //methods
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }//end class
 }
